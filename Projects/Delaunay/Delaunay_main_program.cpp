475e6d4d2f039bc6d3499359a660658b9acdf5bd
Alessandro <aledigrazia9@gmail.com>
2023-06-18 16:20:35 +0200

    ultimo pull

--- Projects/Delaunay/Delaunay_main_program.cpp
+++ Projects/Delaunay/Delaunay_main_program.cpp
@@ -59,7 +59,7 @@ int main() {
 
 
     Reader r;
-    vector<Point> points = r.MakeVector("/Users/gabry/Desktop/Progetto_PCS/Projects/Delaunay/Dataset/prova_punti.txt");
+    vector<Point> points = r.MakeVector("./Dataset/Test2.csv");
     Triangulation t;
     t.Delaunator(points);
     cout<<t;
